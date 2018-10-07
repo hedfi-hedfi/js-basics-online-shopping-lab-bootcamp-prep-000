77d5d52e02ca2a2f83f56e49019edc16d32f6bd5
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 23:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var object = {};
+  object.itemName = item;
+  object.itemPrice = Math.floor(Math.random()*100 + 1);
+  cart.push(object);
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(!cart.length) return 'Your shopping cart is empty.'
+  var list = []
+  for(let i = 0; i < cart.length; i++) {
+    list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`) 
+  }
+  if(cart.lenght > 2){
+    var x = list.pop();
+    return `In your cart, you have ${list.join(", ")} and.`;
+    
+  }
+  else return `In your cart, you have ${list.join(', ')}.`;
 }
 
 function total() {
