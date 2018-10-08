42ad7cd8eb5311c733d68472e242b29a4b433711
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-08 00:17:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,5 @@ function placeOrder(cardNumber) {
     cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
-  
+  console.log("Sorry, we don't have a credit card on file for you");
 }
