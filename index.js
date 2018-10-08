216910b83912027e8119b80ff0abb3425454d9f9
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-08 00:05:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ function viewCart() {
 
 function total() {
   var total = 0.0;
-  for(let i = 0; i < cart.lenght; i++) {
-    total = total + cart[i].itemPrice;
+  for(let i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
   }
   return total;
 }
